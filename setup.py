2b6e88f0001c25aa87e823a4ade1ec2bab5cf821
Sam Hames <sam.hames@qut.edu.au>
2021-10-13 13:23:45 +1000

    Add basic setup.py - to be amended in future

---
+++ setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+install_requires = [
+    "click>=8.0.0",
+    "regex>=2021.3.17",
+]
+
+extras_require = {"development": ["nox", "pytest", "pip-tools"]}
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="twittersphere",
+    author="QUT Digital Observatory",
+    author_email="digitalobservatory@qut.edu.au",
+    description="Tools for content based filtering of tweets and Twitter accounts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/QUT-Digital-Observatory/twittersphere",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Sociology",
+    ],
+    keywords="social_science social_media_analysis",
+    python_requires=">=3.7",
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    entry_points={"console_scripts": ["twittersphere=twittersphere:twittersphere"]},
+)
